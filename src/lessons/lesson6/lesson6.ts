d863784cc8523396b68c8577171252bb8f8df3d6
Mark Kadiner <dasistmail765@gmail.com>
2021-03-14 14:54:13 +0300

    lesson 6 added

--- src/lessons/lesson6/lesson6.ts
+++ src/lessons/lesson6/lesson6.ts
@@ -1,3 +1,5 @@
+// import { Interface } from 'readline';
+
 console.log('Lesson 6');
 
 // Class
@@ -7,41 +9,253 @@ console.log('Lesson 6');
 // https://www.youtube.com/watch?v=BASquaxab_w
 // https://www.youtube.com/watch?v=uLY9GXGMXaA
 
-// Class is defined with capital letter. This is an agreement.
+// class Test {
+//     name: string;
+//
+//     constructor(name: string) {
+//         this.name = name;
+//     }
+//
+// }
+//
+// let test = new Test('Evgen');
+// console.log(test)
 
+// interface IPerson {
+//     name: string;
+//     age: number;
+// }
+//
+// class Person implements IPerson {
+//     name: string;
+//     age: number;
+//     greeting: () => void;
+//
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//         this.greeting = function () {
+//             console.log('greeting');
+//         }
+//     }
+//
+//     hi() {
+//         console.log('hi')
+//     }
+//
+//      bye = () => {
+//          console.log('bye')
+//      }
+// }
+//
+// let human = new Person('Evgen', 32);
+// let human2 = new Person('Vlad', 32);
+// console.log(human)
+// console.log(human2)
+//
+// console.log(human.hi === human2.hi);
+// console.log(human.greeting === human2.greeting);
+// console.log(human.bye === human2.bye);
 
+// interface IPerson {
+//     name: string;
+//     age: number;
+// }
+//
+// class Person implements IPerson {
+//     name: string;
+//     age: number;
+//
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//
+//     greeting() {
+//         console.log('greeting from parent');
+//     }
+//
+//     Yo() {
+//         console.log('Yo');
+//     }
+// }
+//
+// interface IPolitePerson extends IPerson {
+//     greeting: Function;
+// }
+//
+// interface IBye {
+//     bye: Function;
+// }
+//
+// class PolitePerson extends Person implements IPolitePerson, IBye {
+//     constructor(name: string, age: number) {
+//         super(name, age);
+//     }
+//
+//     greeting() {
+//         console.log('greeting');
+//         super.greeting();
+//     }
+//     bye() {
+//         console.log('bye');
+//     }
+// }
+//
+// let human = new PolitePerson('Evgen', 32);
+// console.log(human);
+// human.greeting();
 
-interface IPersonClass {
-    name: string;
-    age: number;
-    city: string;
-}
+// class Test {
+//     name: string;
+//     //@ts-ignore
+//     static key = 10;
+//     //@ts-ignore
+//     #param = 10;
+//     //@ts-ignore
+//     #param2 = 50;
+//     private key2 = 5;
+//     constructor(name: string) {
+//         this.name = name;
+//     }
+// }
+//
+// let human = new Test('Evgen');
+// //@ts-ignore
+// human.key2 = 50;
+// //@ts-ignore
+// human['#param'] = 100;
+// console.log(human)
+// console.log(Test.key)
+// //@ts-ignore
+// window.test = human;
 
-class Person implements IPersonClass {
-    name: string;
-    age: number;
-    city: string;
-
-    constructor(name: string, age: number, city: string) {
-        this.name = name;
-        this.age = age;
-        this.city = city;
+class T {
+    constructor() {
+    }
+    Yo() {
+        console.log('Yo')
     }
 }
 
-const personObject = new Person('Mark', 30, 'SPB')
-console.log(personObject)
+class TTT extends T {
+    constructor() {
+        super();
+    }
+    Yo = () => {
+        console.log('YoYoYo');
+        super.Yo();
+    }
+}
 
+let t = new TTT();
+console.log('t', t)
+t.Yo();
 
 // Task 01
 // Создайте структуру с именем student, содержащую поля: имя и фамилия, номер группы, успеваемость (массив из пяти элементов).
 // Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла.
 // Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
 
+interface IStudent {
+    name: string;
+    surname: string;
+    groupNumber: number;
+    progress: number[];
+    averageMark: number;
+}
+
+class Student implements IStudent {
+    name: string;
+    surname: string;
+    groupNumber: number;
+    progress: number[];
+    averageMark: number;
+
+    constructor(name: string, surname: string, groupNumber: number, progress: number[]) {
+        this.name = name;
+        this.surname = surname;
+        this.groupNumber = groupNumber;
+        this.progress = progress;
+        this.averageMark = this.progress.reduce((sum, mark) => sum + mark) / this.progress.length;
+    }
+
+    private static sortStudent(s1: Student, s2: Student) {
+        if (s1.averageMark > s2.averageMark) {
+            return 1;
+        } else if (s1.averageMark < s2.averageMark) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    static sort(arr:Array<Student>) {
+        const temp = [...arr];
+        return temp.sort(this.sortStudent);
+    }
+
+    private static isAllMarksEqual(marks: number[], mark: number) {
+        return marks.every(item => item === mark);
+    }
+
+    private static filterStudent(arr:Array<Student>) {
+        const result: Array<Student> = [];
+        arr.forEach(item => {
+            if(this.isAllMarksEqual(item.progress, 4) || this.isAllMarksEqual(item.progress, 5)) {
+                result.push(item);
+            }
+        });
+        return result;
+    }
+
+    static printGoodStudent(arr:Array<Student>) {
+        this.filterStudent(arr).forEach(s => {
+            console.log(`Studen - ${s.surname}, Group - ${s.groupNumber}`);
+        });
+    }
+}
+
+let students:Array<Student> = [];
+
+students.push(new Student('Eugene', 'Sheuchuk', 1, [4,4,4,4,4]));
+students.push(new Student('Vlad', 'Bizin', 2, [5,5,5,5,5]));
+students.push(new Student('Hanna', 'Kicel', 3, [4,5,4,5,5]));
+students.push(new Student('Nick', 'Stone', 4, [3,4,3,5,3]));
+students.push(new Student('Alex', 'Page', 5, [3,4,3,3,4]));
+students.push(new Student('Test', 'Testovich', 6, [3,5,3,5]));
+
+//console.log(Student.sort(students));
+Student.printGoodStudent(students);
+
+
 // Task 02
 // Создать класс с двумя переменными. Добавить конструктор с входными параметрами и инициализирующий члены класса по умолчанию.
 // Можно ли создать метод на экземпляре класса который будет удалять сам экземпляр класса?
 // Можно ли создать метод класса который будет удалять экземпляр класса?
+
+// type TT = {
+//     param1: any;
+//     param2: any;
+// }
+//
+// type TT2 = {
+//     param3: any;
+// }
+//
+// type TT3 = TT & TT2;
+//
+// class Task2 implements TT3 {
+//     param1: any;
+//     param2: any;
+//     constructor(param1: any = 10, param2: any = null ) {
+//         this.param1 = param1;
+//         this.param2 = param2;
+//     }
+// }
+
+
+
 
 // Task 03
 // Составить описание класса для представления времени. Предусмотреть возможности установки времени и изменения его отдельных
